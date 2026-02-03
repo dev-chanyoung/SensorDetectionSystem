affa1266f81aa9d03ebb400a85a11b5042526d81
dev-chanyoung <dev.chanyoung@gmail.com>
2026-02-03 16:45:56 +0900

    스프링부트 초기 설정

---
+++ src/main/java/me/devchanyoung/sensordetectionsystem/SensorDetectionSystemApplication.java
@@ -0,0 +1,11 @@
+package me.devchanyoung.sensordetectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDetectionSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SensorDetectionSystemApplication.class,args);
+    }
+}
